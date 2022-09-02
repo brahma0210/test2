e52e06016a6d0626d76906e41efbb6afb94687f4
Brahma reddy Kakunuri <90281219+brahma0210@users.noreply.github.com>
2022-09-01 21:44:16 -0400

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,19 @@
+package test1;
+
+public class Test1  {
+
+	public static void main(String args[]) {
+		{
+			for(int j =1;j<=4;j++)
+			{
+	
+				for(int i =1; i<=4; i++)
+					{
+					System.out.print("1");
+					}
+				System .out.println();
+			}
+		
+		}
+	}
+}
